4084a68a939462d41372f25dad9fab9017ced91f
Madcheese101 <40042269+Madcheese101@users.noreply.github.com>
2024-04-17 15:38:59 +0200

    FEAT: Create Recieve Items at store Doctype

---
+++ renting_services/renting_services/doctype/store_recieve_item/store_recieve_item.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, MadCheese and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Store Recieve Item', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ renting_services/renting_services/doctype/store_recieve_item/test_store_recieve_item.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, MadCheese and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestStoreRecieveItem(FrappeTestCase):
+	pass
