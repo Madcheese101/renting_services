ce3a9ba46a1c3f30adba57976dc1d30789a96d15
Madcheese101 <40042269+Madcheese101@users.noreply.github.com>
2024-04-18 20:05:05 +0200

    on_cancel and on_submit logic for store_recieve_item doctype

--- renting_services/renting_services/doctype/store_recieve_item/store_recieve_item.py
+++ renting_services/renting_services/doctype/store_recieve_item/store_recieve_item.py
@@ -1,8 +1,19 @@
 # Copyright (c) 2024, MadCheese and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import get_fullname
 
 class StoreRecieveItem(Document):
-	pass
+	def on_submit(self):
+		if self.invoice_id:
+			frappe.db.set_value("Sales Invoice", self.invoice_id, "rent_status", 'جاهز')
+
+	def on_cancel(self):
+		if self.invoice_id:
+			frappe.db.set_value("Sales Invoice", self.invoice_id, "rent_status", 'صيانة')
+		
+		# TO-DO edit cleaning doc after cancel
+		if self.repair_id:
+			pass
