dfd2199bb74327baecdb5c6d7e3837c9c78dfb25
Madcheese101 <40042269+Madcheese101@users.noreply.github.com>
2024-04-19 03:24:16 +0200

    implement (confirm/accept recieve  items at store) code

--- renting_services/renting_services/doctype/store_recieve_item/store_recieve_item.js
+++ renting_services/renting_services/doctype/store_recieve_item/store_recieve_item.js
@@ -2,7 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Store Recieve Item', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		const doc = frm.doc;
+		const _frm = frm;
+		if(doc.total_ready !== doc.total_qty && doc.status!=='إنتظار'){
+			// complete finish
+			frm.add_custom_button(
+				__("إستلام"),
+				() => frappe.call({doc: doc,
+					method: "recieve_at_store",
+					callback: function(r){
+						_frm.reload_doc();
+						refresh_field("items");
+				}}),
+				__("إجراءات")
+			);
+			
+		}
+		frm.page.set_inner_btn_group_as_primary(__("إجراءات"));
+	}
 });
--- renting_services/renting_services/doctype/store_recieve_item/store_recieve_item.py
+++ renting_services/renting_services/doctype/store_recieve_item/store_recieve_item.py
@@ -4,6 +4,7 @@
 import frappe
 from frappe.model.document import Document
 from frappe.utils import get_fullname
+from erpnext.selling.page.point_of_sale.point_of_sale import get_pos_profile_data
 
 class StoreRecieveItem(Document):
 	def on_submit(self):
@@ -17,3 +18,45 @@ class StoreRecieveItem(Document):
 		# TO-DO edit cleaning doc after cancel
 		if self.repair_id:
 			pass
+
+	def set_user(self):
+			_user = frappe.session.user
+			full_name = get_fullname(_user)
+			self.db_set('employee', _user)
+			self.db_set('employee_name', full_name)
+
+	@frappe.whitelist()
+	def recieve_at_store(self):
+		profileslist = frappe.db.get_list("POS Profile", filters={"disabled": 0},pluck="name")
+		profile_data = None
+		if profileslist:
+			profile_data =  get_pos_profile_data(profileslist[0])
+			target_wh = profile_data["warehouse"]
+			warehouse = frappe.db.get_value('Warehouse', target_wh, 'reserve_warehouse')
+			
+			stock_entry = frappe.new_doc("Stock Entry")
+			# Set the necessary fields
+			stock_entry.stock_entry_type = "خروج"
+			stock_entry.company = profile_data["company"]
+			stock_entry.flags.ignore_permissions=True
+
+			for item in self.get("items"):
+				child_item = frappe.get_doc("Process Items", item.get("name"))
+				child_item.ready_qty = child_item.qty
+				child_item.flags.ignore_validate_update_after_submit = True
+				child_item.save(ignore_permissions=True)
+
+				stock_entry.append({"item_code": item.item_code,
+						"s_warehouse": warehouse,
+						"t_warehouse":target_wh,
+						"qty":item.qty,
+						})
+			
+			stock_entry.save()
+			stock_entry.submit()
+
+			if self.invoice_id:
+				frappe.db.set_value("Sales Invoice", self.invoice_id, "rent_status", 'جاهز')
+			
+			self.db_set("status", "تم الإستلام")
+			self.set_user()
