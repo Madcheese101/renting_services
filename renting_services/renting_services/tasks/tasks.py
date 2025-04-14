2e5e7a86b862cbb42a978b879c11377ed3965db5
Madcheese101 <40042269+Madcheese101@users.noreply.github.com>
2025-04-14 14:34:49 +0200

    group (advanced payment) reconcilation by Cost Center

--- renting_services/renting_services/tasks/tasks.py
+++ renting_services/renting_services/tasks/tasks.py
@@ -56,73 +56,80 @@ def check_year_old_unallocated_payments():
             # "payment_cleared":0,
             "posting_date":["<=", year_from_now]}
     result =  frappe.get_all("Payment Entry", 
-                             fields=fields, 
-                             filters=filters, 
-                             order_by="posting_date desc") or []
+                fields=fields, 
+                filters=filters, 
+                order_by="posting_date desc") or []
 
     if not result:
         return False
     
-    total = 0
-    je = frappe.new_doc("Journal Entry")
-    je.voucher_type = "Journal Entry"
-    je.user_remark = "التسوية الشهرية للعرابين الأكبر من سنة"
-    je.posting_date = today
-    for payment_entry in result:
-        total += payment_entry.unallocated_amount
+    per_cost_center = {}
+    for payment in result:
+        if not payment.cost_center: continue
+        if payment.cost_center not in per_cost_center.keys() : per_cost_center[payment.cost_center] = []
+        per_cost_center[payment.cost_center].append(payment)
+    
+    for cost_center_, payments in per_cost_center.items():
+        total = 0
+        je = frappe.new_doc("Journal Entry")
+        je.voucher_type = "Journal Entry"
+        je.user_remark = "التسوية الشهرية للعرابين الأكبر من سنة"
+        je.posting_date = today
+        for payment_entry in payments:
+            total += payment_entry.unallocated_amount
+            je.append("accounts", {
+                "account": payment_entry.paid_from,
+                "party_type": "Customer",
+                "party": payment_entry.party,
+                "credit_in_account_currency": 0,
+                "debit_in_account_currency": payment_entry.unallocated_amount,
+                "cost_center": payment_entry.cost_center})
+
         je.append("accounts", {
-            "account": payment_entry.paid_from,
-            "party_type": "Customer",
-            "party": payment_entry.party,
-            "credit_in_account_currency": 0,
-            "debit_in_account_currency": payment_entry.unallocated_amount,
-            "cost_center": payment_entry.cost_center})
+            "account": advance_payment_clearance_account,
+            "credit_in_account_currency": total,
+            "debit_in_account_currency": 0,
+            "cost_center": cost_center})
+        je.title = _("التسوية الشهرية للعرابين") + " " + cost_center_
+        je.ignore_permissions=True
+        je.insert()
+        je.submit()
 
-    je.append("accounts", {
-        "account": advance_payment_clearance_account,
-        "credit_in_account_currency": total,
-        "debit_in_account_currency": 0,
-        "cost_center": cost_center})
-    je.title = _("التسوية الشهرية للعرابين")
-    je.ignore_permissions=True
-    je.insert()
-    je.submit()
+        frappe.db.set_value("Journal Entry", je.name, "pay_to_recd_from", None)
 
-    frappe.db.set_value("Journal Entry", je.name, "pay_to_recd_from", None)
-
-    journal = je.name
-    journal_date = je.posting_date
-    for pe in result:
-        # frappe.db.set_value("Payment Entry", pe.name, "payment_cleared", 1)
-        # Create a Payment Reconciliation document
-        pr = frappe.get_doc({
-            "doctype": "Payment Reconciliation",
-            "company": company,
-            "party": pe.party,  # Replace with actual customer name
-            "party_type": "Customer",
-            "receivable_payable_account": receivable_payable_account,
-            "invoices": [
-                {
-                    "invoice_type": "Journal Entry",
-                    "invoice_number": journal,  # Replace with actual invoice number
-                    "invoice_date": journal_date,  # Replace with actual invoice date
-                    "outstanding_amount": pe.unallocated_amount
-                }
-            ],
-            "payments": [
-                {
-                    "reference_type": "Payment Entry",
-                    "reference_name": pe.name,  # Replace with actual payment entry name
-                    "posting_date": pe.posting_date,  # Replace with actual posting date
-                    "amount": pe.unallocated_amount
-                }
-            ]
-        })
+        journal = je.name
+        journal_date = je.posting_date
+        
+        for pe in payments:
+            # Create a Payment Reconciliation document
+            pr = frappe.get_doc({
+                "doctype": "Payment Reconciliation",
+                "company": company,
+                "party": pe.party,  # Replace with actual customer name
+                "party_type": "Customer",
+                "receivable_payable_account": receivable_payable_account,
+                "invoices": [
+                    {
+                        "invoice_type": "Journal Entry",
+                        "invoice_number": journal,  # Replace with actual invoice number
+                        "invoice_date": journal_date,  # Replace with actual invoice date
+                        "outstanding_amount": pe.unallocated_amount
+                    }
+                ],
+                "payments": [
+                    {
+                        "reference_type": "Payment Entry",
+                        "reference_name": pe.name,  # Replace with actual payment entry name
+                        "posting_date": pe.posting_date,  # Replace with actual posting date
+                        "amount": pe.unallocated_amount
+                    }
+                ]
+            })
 
 
-        if len(pr.invoices) > 0 and len(pr.payments) > 0:
-            invoices = [x.as_dict() for x in pr.invoices]
-            payments = [x.as_dict() for x in pr.payments]
-            pr.allocate_entries(({"invoices": invoices, "payments": payments}))
-        pr.reconcile() #skip_ref_details_update_for_pe=True
-        frappe.db.commit()
+            if len(pr.invoices) > 0 and len(pr.payments) > 0:
+                invoices = [x.as_dict() for x in pr.invoices]
+                payments = [x.as_dict() for x in pr.payments]
+                pr.allocate_entries(({"invoices": invoices, "payments": payments}))
+            pr.reconcile() #skip_ref_details_update_for_pe=True
+            frappe.db.commit()
